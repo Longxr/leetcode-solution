3920cde09823be7bb3a9168adbad2bfb0b486f0c
Longxr <longxuanren@gmail.com>
2020-04-28 23:12:20 +0800

    Create 91.解码方法.cpp

---
+++ 91.解码方法.cpp
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=91 lang=cpp
+ *
+ * [91] 解码方法
+ *
+ * https://leetcode-cn.com/problems/decode-ways/description/
+ *
+ * algorithms
+ * Medium (23.28%)
+ * Likes:    358
+ * Dislikes: 0
+ * Total Accepted:    44.6K
+ * Total Submissions: 190.3K
+ * Testcase Example:  '"12"'
+ *
+ * 一条包含字母 A-Z 的消息通过以下方式进行了编码：
+ * 
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ * 
+ * 
+ * 给定一个只包含数字的非空字符串，请计算解码方法的总数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "12"
+ * 输出: 2
+ * 解释: 它可以解码为 "AB"（1 2）或者 "L"（12）。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "226"
+ * 输出: 3
+ * 解释: 它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+ * 
+ * 
+ */
+
+// @lc code=start
+// class Solution {//递归版,超时艹...
+// public:
+//     int numDecodings(string s) {
+//       return decode(s, s.length() - 1);
+//     }
+    
+//     int decode(string& s, int index) {
+//         if (index <= 0) {
+//           return s[0] == '0' ? 0 : 1;
+//         }
+//         int count = 0;
+        
+//         char cur = s[index];
+//         char pre = s[index - 1];
+        
+//         if (cur > '0') {
+//             count = decode(s, index - 1);
+//         }
+        
+//         if (pre == '1' || (pre == '2' && cur <= '6')) {
+//             count += decode(s, index - 2);
+//         }
+//         return count;
+//     }
+// };
+
+class Solution {//动态规划
+public:
+  int numDecodings(string s) {
+    if (s[0] == '0') return 0;
+      int pre = 1, curr = 1;//dp[-1] = dp[0] = 1
+      for (int i = 1; i < s.size(); i++) {
+          int tmp = curr;
+          if (s[i] == '0')
+              if (s[i - 1] == '1' || s[i - 1] == '2') 
+                curr = pre; //dp[i]=dp[i-2]
+              else 
+                return 0;
+          else if (s[i - 1] == '1' || (s[i - 1] == '2' && s[i] >= '1' && s[i] <= '6'))
+              curr = curr + pre;//dp[i]=dp[i-1] + dp[i-2]
+          pre = tmp;
+      }
+      return curr;
+    }
+};
+// @lc code=end
+
