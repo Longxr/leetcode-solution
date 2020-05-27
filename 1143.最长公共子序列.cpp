64af77fea0ea385fff10c14ee799bb15c6a4f5f1
Longxr <longxuanren@gmail.com>
2020-05-27 22:49:30 +0800

    Update 1143.最长公共子序列.cpp

--- 1143.最长公共子序列.cpp
+++ 1143.最长公共子序列.cpp
@@ -67,10 +67,10 @@ public:
 
       for (int i = 1; i < text1.size() + 1; i++) {
         for (int j = 1; j < text2.size() + 1; j++){
-          if (text1[i-1] == text2[j-1]) {
-            dp[i][j] = dp[i - 1][j - 1] + 1;
-          } else {
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+          if (text1[i-1] == text2[j-1]) { //末端相同，降为子问题+1
+            dp[i][j] = dp[i - 1][j - 1] + 1;
+          } else {              //末端不同，分别求两个子问题最大值
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
           }
         }
       }
